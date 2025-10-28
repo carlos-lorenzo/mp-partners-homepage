e58f328ede7344f4be2695f3703977eba6df6898
Carlos Lorenzo <clorenzozuniga@gmail.com>
2025-10-28 15:25:33 +0100

    - included founders in about section - fixed about page's responsiveness

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -8,8 +8,8 @@ export default function About() {
             <h1 id="about-title">Our Journey</h1>
             <h2 id="about-subtitle">From Foundation to Future</h2>
             <div className="timeline">
-                <TimelineItem date="2025" title="Founded" side="left">
-                    <p>MP Partners was founded by Bocconi University students with the goal of supporting companies, start-ups, and alternative investment operators, particularly within the sports, media, and innovation industries, in developing strategic and growth-oriented projects.</p>
+                <TimelineItem date="September 2025" title="Founded" side="left">
+                    <p>MP Partners was founded by Bocconi University students <a href='https://www.linkedin.com/in/alnicolau/' target='_blank'>Alvaro Nicolau</a> and <a href='https://www.linkedin.com/in/antonio-di-paolo/' target='_blank'>Antonio Di Paolo</a> with the goal of supporting companies, start-ups, and alternative investment operators, particularly within the sports, media, and innovation industries, in developing strategic and growth-oriented projects.</p>
                 </TimelineItem>
                 <TimelineItem date="Present" title="Our Impact" side="right">
                     <p>As part of our track record, we have already launched collaborations in the sports industry, including a project developed with Open Field Advisory, where we supported the firm in identifying Italian companies interested in partnerships with American sports teams (NFL), as well as potential collaborations for related events in Italy and the U.S., contributing to market research, investor relations, and deal execution.</p>
